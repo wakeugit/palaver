02383297111ec119d14724b5d665abfea6d3f570
micen <michael.enov@gmail.com>
2017-07-18 22:49:27 +0200

    Unnötige Klassen entfernt + Notification samt Sound

--- app/src/main/java/mobile/paluno/de/palaver/gcm/PalaverGcmListenerService.java
+++ app/src/main/java/mobile/paluno/de/palaver/gcm/PalaverGcmListenerService.java
@@ -1,24 +1,13 @@
 package mobile.paluno.de.palaver.gcm;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
-import mobile.paluno.de.palaver.controller.ChatHistoryActivity;
-
-import static android.support.v4.app.NotificationCompat.VISIBILITY_PUBLIC;
-
-
 public class PalaverGcmListenerService extends GcmListenerService {
 
-    public static boolean isLoggedIn =true;
+    public static boolean isLoggedIn = true;
 
     @Override
     public void onMessageReceived(String s, Bundle bundle) {
